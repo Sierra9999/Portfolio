b667dba3fb1eed9b5b5c718cf5b03f0f377d4deb
Sierra <m.sierra1037@hotmail.com>
2023-07-08 09:42:23 -0500

    feat: reading posts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,16 @@
+import fs from "fs"
+
+const getPostMetaData = () => { 
+    const folder = "posts/"
+    const files = fs.readdirSync(folder)
+    
+
+    return files
+ }
 
 export default function Home() {
 
+  console.log(getPostMetaData());
   return (
     <main >
       <h1>Greetings 🤡</h1>
