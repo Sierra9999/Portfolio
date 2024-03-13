8918270d89f3180f2b82d012ab94539239b7143e
Mateo Sierra <m.sierra1037@hotmail.com>
2024-03-13 12:03:42 -0500

    feat: hero text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import Hero from "@/components/Hero";
 import Portfolio from "@/components/Portfolio";
 
 export default function Home() {
   return (
     <main>
+      <Hero/>
       <Portfolio/>
     </main>
   );
---
+++ src/components/Hero.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <h1></h1>
+      <div className="hero__text slide-from-left">
+        <h1>FRONTEND DEVELOPER. BASED IN MEDELLIN.</h1>
+        <h3>FRONTEND SHOPIFY NEXT.JS</h3>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
