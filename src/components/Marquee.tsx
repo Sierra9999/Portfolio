4711f21d1f5ac8a5cade1125e614d7f730dd7105
Mateo Sierra <m.sierra1037@hotmail.com>
2024-03-14 11:20:22 -0500

    fix: removed error inducing array

--- src/components/Marquee.tsx
+++ src/components/Marquee.tsx
@@ -1,12 +1,6 @@
 import React from "react";
 
 const Marquee = () => {
-
-    const displayedElements = [
-    <h1> marquee component </h1>,
-    <h1> marquee component </h1>,
-    <h1> marquee component </h1>]
-
   return (
     <div className="marquee">
       <div className="marquee__slider">
