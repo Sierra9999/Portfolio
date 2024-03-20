ef29efc207aaf88e3a2cb937f2b5ec8d923a0685
Mateo Sierra <m.sierra1037@hotmail.com>
2024-03-20 16:27:44 -0500

    feat: social icons

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Socials from './Socials'
 
 const Footer = () => {
   return (
@@ -6,7 +7,7 @@ const Footer = () => {
         <h1 className='footer__title'>LET&apos;S CONNECT</h1>
         <p className='footer__message'>Do you have a project in mind? </p>
         <a href="mailto:m.sierra1037@hotmail.com" className='footer__cta get-in-touch'>let&apos;s get in touch!</a>
-
+        <Socials/>
     </footer>
   )
 }
---
+++ src/components/Socials.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { SocialIcon } from "./Icons";
+const Socials = () => {
+  return (
+    <div className="socials">
+      <a target="_blank" href="https://www.linkedin.com/in/mateo-sierra-4b70951b7/">
+        <SocialIcon iconName="linkedin" />
+      </a>
+      <a target="_blank" href="https://github.com/Sierra9999 ">
+      <SocialIcon iconName="github" />
+      </a>
+
+    </div>
+  );
+};
+
+export default Socials;
