78f69f529ce39eef5edfe42fdad2cdabb540cfad
Mateo Sierra <m.sierra1037@hotmail.com>
2024-04-03 08:30:55 -0500

    feat: hero animations

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,10 +5,10 @@ const Hero = () => {
   return (
     <section className="hero">
       <div className="hero__content">
-        <h1 className="hero__name"> Mateo Sierra.</h1>
-        <h1 className="hero__focusing">Frontend Developer</h1>
-        <h1 className="hero__focusing">shopify Developer</h1>
-        <h1 className="hero__location">Based in <span className="red-gradient">Medellin.</span></h1>
+        <h1 className="hero__name slide-from-left"> Mateo Sierra.</h1>
+        <h1 className="hero__focusing slide-from-right blue-gradient">Frontend Developer &</h1>
+        <h1 className="hero__focusing slide-from-right green-gradient">shopify Developer</h1>
+        <h1 className="hero__location slide-from-bottom animation-duration-2">Based in <span className="red-gradient">Medellin.</span></h1>
       </div>
       <a href="#contact">let&apos;s connect</a>
       <div className="hero__marquee">
