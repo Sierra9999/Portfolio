afe56f3d7e4114fa8ee22146252ae41620dfe466
Mateo Sierra <m.sierra1037@hotmail.com>
2024-04-03 08:17:01 -0500

    feat: new hero look

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,20 +1,19 @@
 import React from "react";
+import Marquee from "./Marquee";
 
 const Hero = () => {
   return (
     <section className="hero">
-        <h1 className="hero__name"> Mateo Sierra.
-          
-        </h1>
-        <h1 className="hero__focusing"><span>Focusing</span> <span>On</span></h1>
-        <div className="hero__bottom">
-          <div className="hero__services">
-              <h2 className="service ">Next.js</h2>
-              <h2 className="service green-gradient">Shopify</h2>
-              <h2 className="service blue-gradient">React </h2>
-          </div>
-          <h1 className="hero__bottom-text">Web Solutions</h1>
-        </div>
+      <div className="hero__content">
+        <h1 className="hero__name"> Mateo Sierra.</h1>
+        <h1 className="hero__focusing">Frontend Developer</h1>
+        <h1 className="hero__focusing">shopify Developer</h1>
+        <h1 className="hero__location">Based in <span className="red-gradient">Medellin.</span></h1>
+      </div>
+      <a href="#contact">let&apos;s connect</a>
+      <div className="hero__marquee">
+        <Marquee></Marquee>
+      </div>
     </section>
   );
 };
