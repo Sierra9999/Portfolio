b283283a3c2209e49eac70a604c1a58b3f844dab
Mateo Sierra <m.sierra1037@hotmail.com>
2024-04-03 17:50:20 -0500

    fix: responsive font

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,7 +5,9 @@ const Hero = () => {
   return (
     <section className="hero">
       <div className="hero__content">
-        <h1 className="hero__name slide-from-left"> Mateo Sierra <span className="bounce red-gradient">.</span></h1>
+        <h1 className="hero__name slide-from-left"> 
+          Mateo Sierra <span className="bounce red-gradient">.</span>
+        </h1>
         <h1 className="hero__focusing slide-from-right blue-gradient">Frontend Developer</h1>
         <h1 className="hero__focusing slide-from-right green-gradient">shopify Developer</h1>
         <h1 className="hero__location slide-from-bottom animation-duration-2">Based in <span className="red-gradient">Medellin.</span></h1>
