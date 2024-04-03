3da745b957422cb5d3e1ee73db2e9875b8d9ac27
Mateo Sierra <m.sierra1037@hotmail.com>
2024-04-03 11:53:16 -0500

    fix: header simplified

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,28 +2,24 @@ import React from "react";
 
 const Header = () => {
   return (
-    <header>
-      <div className="slide-from-top top-section">
-        <h1>Mateo Sierra</h1>
-      </div>
-      <nav className="main-navigation">
-        <ul>
-          <li>
-            <a href="">About</a>
-          </li>
-        </ul>
-        <ul>
-          <li>
-            <a href="#portfolio">Portfolio</a>
-          </li>
-        </ul>
-        <ul>
-          <li>
-            <a href="#contact">Contact</a>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <>
+      <header>
+        <div className="slide-from-top top-section">
+        </div>
+        <nav className="main-navigation">
+          <ul>
+            <li>
+              <a href="#portfolio">Portfolio,</a>
+            </li>
+          </ul>
+          <ul>
+            <li>
+              <a href="#contact">Contact,</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
   );
 };
 
