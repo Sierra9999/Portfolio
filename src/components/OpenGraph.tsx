046e613ab904fb1c1c6a69bf01cc7e1f4781ad73
Mateo Sierra <m.sierra1037@hotmail.com>
2024-04-08 16:14:54 -0500

    feat: ogp description changed

--- src/components/OpenGraph.tsx
+++ src/components/OpenGraph.tsx
@@ -1,17 +1,18 @@
 import React from "react";
 
 const OpenGraph = () => {
-  const { description, domain, imgUrl } = {
+  const { description, domain, imgUrl ,title } = {
     domain: "https://sierramateo.com/",
     description:
-      "Front-end Developer - Building Clean, Beautiful Web Applications",
+      "Explore the creative journey of Mateo Sierra, showcasing a portfolio filled with innovative designs and captivating projects. Discover the essence of excellence in every pixel and line.",
     imgUrl:
       "https://cdn.discordapp.com/attachments/1226622215047352351/1226622266717110393/op-thumbnail.jpg?ex=66257001&is=6612fb01&hm=28d213304b755c2632be3abcca6c4c01d4352b5cd9a7f22f5857085441dfd88d&",
+    title : 'Mateo Sierra ⭕',
   };
 
   return (
     <head>
-      <meta property="og:title" content="Mateo Sierra ⭕" />
+      <meta property="og:title" content={title} />
       <meta property="og:type" content="article" />
       <meta property="og:url" content={domain} />
       <meta property="og:description" content={description} />
